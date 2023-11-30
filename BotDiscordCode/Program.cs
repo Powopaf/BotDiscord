8f93fd969965e6e8faf79bf8abae8155a71c6f0e
Powopaf <paf.fournier@gmail.com>
2023-11-30 13:38:40 +0100

    gitigore

--- BotDiscordCode/Program.cs
+++ BotDiscordCode/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace BotDiscordCode;
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace BotDiscordCode;
 
 public class Program
 {
+    private DiscordSocketClient;
     public static void Main(string[] args)
     {
         
     }
+    
+    
 }
